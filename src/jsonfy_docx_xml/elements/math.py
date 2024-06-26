45eb8850e2bc27052a7d58469295f996f560b88d
hdy <977648857@qq.com>
2024-06-26 13:08:57 +0800

    增加embObject的解析

--- src/jsonfy_docx_xml/elements/math.py
+++ src/jsonfy_docx_xml/elements/math.py
@@ -1,7 +1,10 @@
+import base64
 from typing import Dict, Any
 
+from PIL import Image
 from docx.oxml.ns import qn
 from lxml import etree
+from io import BytesIO
 
 from jsonfy_docx_xml.elements import el
 
@@ -28,3 +31,31 @@ class mathParagraph(math):
         omath_element = x.find(qn('m:oMath'))
         self.omml_str= etree.tostring(omath_element, pretty_print=True, encoding='unicode')
         print()
+
+
+class embObject(el):
+    __type__ = "embObject"
+    def __init__(self, x):
+        imagedata_element = x.xpath('.//v:imagedata', namespaces=x.nsmap)
+        imagedata_element = imagedata_element[0]
+        rId = imagedata_element.get('{http://schemas.openxmlformats.org/officeDocument/2006/relationships}id')
+        self.rId = rId
+
+
+    def to_json(self, doc) -> Dict[str, Any]:
+        out =  super().to_json(doc)
+        if self.rId:
+            rel = doc.part.rels[self.rId]
+            image_part = rel.target_part
+            data = image_part.blob
+            ext = rel.target_ref.split('.')[-1]  # 获取扩展名
+            # 通过rId直接获取对应的图像数据
+            # image_part = doc.part.rels.get(self.rId).target_part
+            # image_data = image_part.blob
+
+            # 使用PIL显示图片
+            base64_data = base64.b64encode(data).decode('utf-8')
+
+            temp = {"blob": base64_data, "ext": ext}
+            out.update(temp)
+            return out
