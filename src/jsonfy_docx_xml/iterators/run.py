29f9e81c5dae22667a3773fa914e23d6089ba1fe
hdy <977648857@qq.com>
2024-06-27 09:14:02 +0800

    变基操作过后，我也不知道到哪一步了

--- src/jsonfy_docx_xml/iterators/run.py
+++ src/jsonfy_docx_xml/iterators/run.py
@@ -2,9 +2,14 @@ from docx.oxml.ns import qn
 
 from jsonfy_docx_xml.elements.image import image
 from jsonfy_docx_xml.elements.math import math, embObject
+from jsonfy_docx_xml.elements.run import commentReference
 from jsonfy_docx_xml.iterators.generic import register_iterator
 
 register_iterator("run",
                   TAGS_TO_YIELD={qn("w:drawing"):image,
                                  qn("m:oMath"):math,
-                                 qn("w:object"):embObject})
+                                 qn("w:object"):embObject,
+                                 qn("w:image"):image,
+                                 qn("w:commentReference"): commentReference
+                                }
+                  )
